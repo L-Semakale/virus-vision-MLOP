863b33e370eaf4580e74ebd0305c801112401fb5
L.Semakale <155956058+L-Semakale@users.noreply.github.com>
2025-11-27 20:42:21 +0200

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from api.predict import predict_image
-from api.retrain import retrain_model_background
-from api.schemas import PredictionResponse, RetrainResponse
+from api.schemas import PredictionResponse
 
 app = FastAPI(title="Virus Vision API", version="1.0")
 
-# CORS (required for UI)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -22,8 +20,3 @@ def root():
 @app.post("/predict", response_model=PredictionResponse)
 async def predict_endpoint(file: UploadFile = File(...)):
     return await predict_image(file)
-
-@app.post("/retrain", response_model=RetrainResponse)
-async def retrain_endpoint():
-    retrain_model_background()
-    return {"status": "Retraining started in the background."}
