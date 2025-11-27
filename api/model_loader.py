9d534096f519b2f422f40b577465aee0fd70ef3b
L.Semakale <155956058+L-Semakale@users.noreply.github.com>
2025-11-27 21:08:28 +0200

    Refactor model loading and remove image preparation
    Updated model loading logic to support weights_only option and removed image preparation function.

--- api/model_loader.py
+++ api/model_loader.py
@@ -1,21 +1,11 @@
 import torch
-from torchvision import transforms
-from PIL import Image
 
 MODEL_PATH = "model/model.pt"
 model = None
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor()
-])
 
 def load_model():
     global model
     if model is None:
-        model = torch.load(MODEL_PATH, map_location="cpu")
-        model.eval()
+        model = torch.load(MODEL_PATH, map_location="cpu", weights_only=False)
+        # No .eval() because this is NOT a neural network
     return model
-
-def prepare_image(file):
-    img = Image.open(file).convert("RGB")
-    return transform(img).unsqueeze(0)
